10ae0c7711da58a9efd3456e67c51fd213d49a1e
Jair Lima de Souza <jairdeveloper2002@gmail.com>
2024-08-11 09:54:39 -0300

    att Order class

---
+++ src/main/java/com/swdev/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.swdev.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
--- src/main/java/com/swdev/dscommerce/entities/User.java
+++ src/main/java/com/swdev/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.swdev.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="tb_user")
@@ -17,6 +19,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders  = new ArrayList<>();
 
     public User() {
     }
