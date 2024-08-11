95bacdc73e82d0ff3a82ddc2968d6f23577bd95c
Jair Lima de Souza <jairdeveloper2002@gmail.com>
2024-08-11 09:39:28 -0300

    add Order class

---
+++ src/main/java/com/swdev/dscommerce/DscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.swdev.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}
